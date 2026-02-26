1f1871dff9f198f9ff3cf8b4195fca63ac2f4ebe
Boazoloo <boazoloo263@gmail.com>
2026-02-26 01:31:36 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,13 @@
 /**
  * main - Entry point
  *
+ * Description: Prints a string
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\"n");
-    return (0);
+	puts("\ "Programming is like building a multilingual puzzle");
+	return (0);
 }
 
