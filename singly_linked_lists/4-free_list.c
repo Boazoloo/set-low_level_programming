65031fe31f1b0270ff27b6fa22a03f8edb901257
Boazoloo <boazoloo263@gmail.com>
2026-05-17 03:24:49 -0700

    done

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -4,17 +4,19 @@
 /**
  * free_list - frees a list_t list
  * @head: pointer to the head of the list
+ *
+ * Return: void
  */
 void free_list(list_t *head)
 {
-    list_t *temp;
+	list_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head->next;
-        free(head->str);
-        free(head);
-        head = temp;
-    }
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
 
