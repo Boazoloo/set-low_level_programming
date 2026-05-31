5d1dc41d63af81bed0f5b93ba54ab0b938ebe147
Boazoloo <boazoloo263@gmail.com>
2026-05-31 06:03:25 -0700

    done

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -3,50 +3,139 @@
 /**
  * insert_nodeint_at_index - inserts a new node at a given index
  * @head: pointer to head pointer
- * @idx: index where new node is added
- * @n: value to store in new node
+ * @idx: index where new node is inserted
+ * @n: value of new node
  *
- * Return: address of new node, or NULL if it failed
+ * Return: address of new node, or NULL if it fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head,
-                                    unsigned int idx, int n)
+	unsigned int idx, int n)
 {
-    listint_t *new_node;
-    listint_t *temp;
-    unsigned int i = 0;
+	listint_t *new_node;
+	listint_t *temp;
+	unsigned int i = 0;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
+	new_node->n = n;
 
-    /* insert at head */
-    if (idx == 0)
-    {
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    temp = *head;
+	temp = *head;
 
-    /* move to node before idx */
-    for (i = 0; temp != NULL && i < idx - 1; i++)
-        temp = temp->next;
+	for (i = 0; temp != NULL && i < idx - 1; i++)
+		temp = temp->next;
 
-    if (temp == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (temp == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->next = temp->next;
-    temp->next = new_node;
+	new_node->next = temp->next;
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
